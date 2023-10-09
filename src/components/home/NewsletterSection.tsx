cd86a4d325bcb74dcd73b65583b6a0fd3d836d63
ArthurBoucard <arthur.boucard@hotmail.fr>
2023-10-09 15:40:56 +0200

    fix import

--- src/components/home/NewsletterSection.tsx
+++ src/components/home/NewsletterSection.tsx
@@ -5,7 +5,6 @@ import Image from "next/image";
 import validator from "validator";
 import { inputErrors } from "@/const";
 import { useStrapi } from "@/hooks/useStrapi";
-import { sendEmail } from '../../lib/sendNewsletterEmail';
 import axios from "axios";
 
 type NewsletterSectionProps = {
