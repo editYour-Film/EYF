813d8fd8fee0327d473eb36e2e134a52a980b541
Mehdi Marouani <mehdi.marouani.developer@gmail.com>
2023-10-18 16:44:05 +0100

    Update Button.tsx

--- src/components/_shared/buttons/Button.tsx
+++ src/components/_shared/buttons/Button.tsx
@@ -1,40 +1,51 @@
 type ButtonProps = {
-  type: 'primary' | 'secondary',
-  label: string,
-  Icon?: () => JSX.Element,
-  disabled?: boolean,
-  className?: string,
-  onClick: () => void,
-}
+  type: "primary" | "secondary";
+  label: string;
+  Icon?: any; //() => JSX.Element,
+  disabled?: boolean;
+  className?: string;
+  onClick: () => void;
+};
 
-export const Button:React.FC<ButtonProps> = ({type, label, Icon, disabled = false, className, onClick}: ButtonProps) => {  
-  const baseStyle = `h-[52px] flex justify-center items-center gap-dashboard-specific-radius px-dashboard-specific-radius border text-dashboard-text-button-white-contrast-low rounded-dashboard-button-square-radius transition-colors duration-200`
-  const focusedStyle = `focus:border-2 focus:border-dashboard-button-focus-stroke`
-  let disabledStyle = `pointer-events-none text-dashboard-text-disabled`
-  
-  let typeStyle
+export const Button: React.FC<ButtonProps> = ({
+  type,
+  label,
+  Icon,
+  disabled = false,
+  className,
+  onClick,
+}: ButtonProps) => {
+  const baseStyle = `h-[52px] flex justify-center items-center gap-dashboard-specific-radius px-dashboard-specific-radius border text-dashboard-text-button-white-contrast-low rounded-dashboard-button-square-radius transition-colors duration-200`;
+  const focusedStyle = `focus:border-2 focus:border-dashboard-button-focus-stroke`;
+  let disabledStyle = `pointer-events-none text-dashboard-text-disabled`;
+
+  let typeStyle;
   switch (type) {
-    case 'primary':
+    case "primary":
       typeStyle = `bg-dashboard-button-dark border-dashboard-button-stroke-default hover:border-dashboard-button-stroke-hover`;
       break;
-    case 'secondary':
+    case "secondary":
       typeStyle = `bg-dashboard-button-white-default border-dashboard-button-dark-border hover:bg-dashboard-button-white-hover focus:bg-dashboard-button-white-hover`;
       break;
   }
 
   return (
-    <button 
-      className={`button ${baseStyle} ${typeStyle} ${focusedStyle} ${disabled ? disabledStyle : ''} ${className ?? ''}`}
-      onClick={() => { onClick(); }}
+    <button
+      className={`button ${baseStyle} ${typeStyle} ${focusedStyle} ${
+        disabled ? disabledStyle : ""
+      } ${className ?? ""}`}
+      onClick={() => {
+        onClick();
+      }}
       disabled={disabled}
     >
-      {Icon && 
-        <Icon 
-          className= {`${disabled ? 'svg-color-dashboard-text-disabled' : ''}`}
+      {Icon && (
+        <Icon
+          className={`${disabled ? "svg-color-dashboard-text-disabled" : ""}`}
         />
-      }
+      )}
 
       {label}
     </button>
-  )
-}
+  );
+};
