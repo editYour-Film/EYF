4ba8c1e05129c05d12862c0be67a306d0339356e
Mehdi Marouani <mehdi.marouani.developer@gmail.com>
2023-10-19 13:16:27 +0100

    Update codePan.tsx

--- src/components/signin/codePan.tsx
+++ src/components/signin/codePan.tsx
@@ -1,85 +1,82 @@
-import { Button } from "@/components/_shared/buttons/Button"
-import { useContext, useEffect, useRef, useState } from "react"
-import { SecretCodeInput } from "../_shared/form/SecretCodeInput"
-import { SignInContext, codeStateType } from "./_context/signinContext"
-import { InfoMessage } from "../_shared/UI/InfoMessage"
-import { MessageCenter } from "../_shared/UI/MessageCenter"
+import { Button } from "@/components/_shared/buttons/Button";
+import { useContext, useEffect, useRef, useState } from "react";
+import { SecretCodeInput } from "../_shared/form/SecretCodeInput";
+import { SignInContext, codeStateType } from "./_context/signinContext";
+import { InfoMessage } from "../_shared/UI/InfoMessage";
 
-import Send from '@/icons/signin/send.svg'
-import X from '@/icons/signin/x.svg'
-import Connexion from '@/icons/signin/connexion.svg'
-import gsap from "gsap"
-import { ElementsIn } from "@/Animations/elementsIn"
+import Send from "@/icons/signin/send.svg";
+import X from "@/icons/signin/x.svg";
+import Connexion from "@/icons/signin/connexion.svg";
+import gsap from "gsap";
+import { ElementsIn } from "@/Animations/elementsIn";
 
 export const CodePan = () => {
-  const container = useRef<HTMLDivElement>(null)
-  const context = useContext(SignInContext)
+  const container = useRef<HTMLDivElement>(null);
+  const context = useContext(SignInContext);
 
   useEffect(() => {
-    const elements = Array.from(container.current!.children)
+    const elements = Array.from(container.current!.children);
 
-    ElementsIn(elements)
-  }, [])
+    ElementsIn(elements);
+  }, []);
 
-  const handleSendAgain = () => {
+  const handleSendAgain = () => {};
 
-  }
+  const handleGoBack = () => {};
 
-  const handleGoBack = () => {
-
-  }
-
-  const switchMessage = (switchVal:codeStateType) => {
+  const switchMessage = (switchVal: codeStateType) => {
     switch (switchVal) {
-      case 'regular':
+      case "regular":
         return (
-          <InfoMessage 
-            message="Ouvrez votre boite mail et ajoutez votre code à 6 chiffres." 
-            Icon={Send} 
+          <InfoMessage
+            message="Ouvrez votre boite mail et ajoutez votre code à 6 chiffres."
+            Icon={Send}
           />
-        )
-      case 'loading':
+        );
+      case "loading":
         return (
-          <InfoMessage 
-            message="Ouvrez votre boite mail et ajoutez votre code à 6 chiffres." 
-            Icon={Send} 
+          <InfoMessage
+            message="Ouvrez votre boite mail et ajoutez votre code à 6 chiffres."
+            Icon={Send}
           />
-        )
-      case 'error':
+        );
+      case "error":
         return (
-          <InfoMessage 
-            type='danger' 
-            message="Code saisi invalide. Rééssayez ou recevez un nouveau code." 
+          <InfoMessage
+            type="danger"
+            message="Code saisi invalide. Rééssayez ou recevez un nouveau code."
             Icon={X}
           />
-        )
-      case 'success':
-        return (
-          <InfoMessage message='Connexion ...' Icon={Connexion}/>
-        )
+        );
+      case "success":
+        return <InfoMessage message="Connexion ..." Icon={Connexion} />;
       default:
         break;
     }
-  }
+  };
 
-  const messageContainer = useRef<HTMLDivElement>(null)
+  const messageContainer = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
-    const tl = gsap.timeline()
+    const tl = gsap.timeline();
 
     tl.to(messageContainer, {
-      opacity: 0
-    })
-
-  }, [context.codeState])
+      opacity: 0,
+    });
+  }, [context.codeState]);
 
   return (
     <div className="signIn_code max-w-[100vw] w-[424px]">
-      <div ref={container} className="flex flex-col items-center justify-center gap-dashboard-spacing-element-medium">
-        <hr className='w-full'/>
-        <div className='text-large px-dashboard-specific-radius md:p-0 text-center'>Vérification de votre compte</div>
-        
-        <div 
+      <div
+        ref={container}
+        className="flex flex-col items-center justify-center gap-dashboard-spacing-element-medium"
+      >
+        <hr className="w-full" />
+        <div className="text-large px-dashboard-specific-radius md:p-0 text-center">
+          Vérification de votre compte
+        </div>
+
+        <div
           ref={messageContainer}
           className="px-dashboard-specific-radius md:p-0"
         >
@@ -88,33 +85,39 @@ export const CodePan = () => {
 
         {/* {switchMessage(context.codeState)} */}
 
-        <SecretCodeInput 
+        <SecretCodeInput
           state={context.codeState}
-          enterCb={(val:string) => { context.handleCodeVerification(val)}}
-          backSpaceCb={() => { context.resetCodeState()}}
+          enterCb={(val: string) => {
+            context.handleCodeVerification(val);
+          }}
+          backSpaceCb={() => {
+            context.resetCodeState();
+          }}
         />
-         
-        <hr className='w-full'/>  
+
+        <hr className="w-full" />
 
         <div className="w-full px-dashboard-specific-radius md:p-0">
           <Button
-            type='secondary'
-            label='Renvoyer le code'
-            onClick={() => { handleSendAgain()}}
+            type="secondary"
+            label="Renvoyer le code"
+            onClick={() => {
+              handleSendAgain();
+            }}
             className="w-full"
-            disabled={context.codeState === 'success'}
+            disabled={context.codeState === "success"}
           />
-          <Button 
-            type='primary'
-            label='Retour'
-            onClick={() => { handleGoBack()}}
+          <Button
+            type="primary"
+            label="Retour"
+            onClick={() => {
+              handleGoBack();
+            }}
             className="w-full mt-dashboard-mention-padding-right-left"
-            disabled={context.codeState !== 'success'}
+            disabled={context.codeState !== "success"}
           />
         </div>
-
       </div>
     </div>
-
-  )
-}
+  );
+};
