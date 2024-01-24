d2183564dd48ba4564191dc1d9edf347663a9a71
robinNavas <robinnavas@gmail.com>
2024-01-24 09:07:29 +0100

    create quote api client

---
+++ src/services/quote/quoteApi.ts
@@ -0,0 +1,36 @@
+import axios from 'axios';
+
+const apiService = axios.create({
+  baseURL: process.env.NEXT_PUBLIC_EYF_API,
+  headers: {
+
+  }
+});
+
+export const createQuote = async () => {  
+  try {
+    const response = await apiService.post('/quotes');
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
+};
+
+interface calculateQuotePrice {
+  complexity: 'basic' | 'medium' | 'complex',
+  rushTotalMinutes: number,
+  videoExpectedMinutes: number
+}
+
+export const calculateQuotePrice = async ({complexity, rushTotalMinutes, videoExpectedMinutes}: calculateQuotePrice) => {  
+  try {
+    const response = await apiService.post('/quotes/calculate', {
+      complexity: complexity,
+      rushTotalMinutes: rushTotalMinutes,
+      videoExpectedMinutes: videoExpectedMinutes
+    });
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
+};
