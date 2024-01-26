66371a72db77fabcf8320fb731a09459192fdd6a
robinNavas <robinnavas@gmail.com>
2024-01-26 15:56:05 +0100

    Change layout + move the searchbar to a component

--- src/components/catalogue/Catalog.tsx
+++ src/components/catalogue/Catalog.tsx
@@ -1,22 +1,25 @@
-import { RefObject, Suspense, forwardRef, useContext, useEffect, useRef, useState } from "react"
-import { QuoteContext, QuoteContextType } from "../quote/_context/QuoteContext"
+import { RefObject, forwardRef, useContext, useEffect, useRef, useState } from "react"
+import { ReactLenis, useLenis } from "@studio-freight/react-lenis"
+import gsap from "gsap"
+import { ScrollTrigger } from "gsap/dist/ScrollTrigger";
+import { useGSAP } from "@gsap/react"
+
+import { useWindowSize } from "@uidotdev/usehooks"
+import useMediaQuery from "@/hooks/useMediaQuery"
+
+import { lockDocumentScroll, unLockDocumentScroll } from "@/utils/utils"
+
+import { CatalogContext, CatalogContextType } from "./_context/CatalogContext"
+
 import { SearchBar } from "../_shared/form/SearchBar"
 import { DropBox } from "../_shared/form/DropBox"
 import { ModelsLine, ModelsLineSkeleton } from "./ModelsLine"
-import FiltersIcon from '@/icons/sliders.svg'
-import gsap from "gsap"
-import { CatalogContext, CatalogContextType } from "./_context/CatalogContext"
 import { SimpleCard } from "../_shared/UI/CardSimple"
 import { IslandButton } from "../_shared/buttons/IslandButton"
-
-import ArrowLeft from '@/icons/arrow-left.svg'
 import { CatalogModel } from "./CatalogModel"
-import { ReactLenis, useLenis } from "@studio-freight/react-lenis"
-import { lockDocumentScroll, unLockDocumentScroll } from "@/utils/utils"
-import Container from "../_shared/UI/Container"
 import { ModelSkeleton } from "../_shared/video/Model"
-import useMediaQuery from "@/hooks/useMediaQuery"
-import { useWindowSize } from "@uidotdev/usehooks"
+
+import FiltersIcon from '@/icons/sliders.svg'
 
 type CatalogProps = {
   searchChoices: CatalogContextType['searchValues'],
@@ -25,20 +28,17 @@ type CatalogProps = {
 }
 
 export const Catalog = ({searchChoices, models, isQuote}: CatalogProps) => {
-
   const catalogContext = useContext(CatalogContext)
-
-  const [filterOpen, setFilterOpen] = useState(false)
-  const [filters, setFilters] = useState<string[]>([])
 
   const [isFetching, setIsFetching] = useState(false)
 
   const mainContent = useRef<HTMLDivElement>(null)
-  const morePanel = useRef<HTMLDivElement>(null)
   const wrapper = useRef<HTMLDivElement>(null)
 
   // Trigger the fetch more when we approch the bottom
-  const lenis = useLenis(() => {
+  useLenis(() => {
+    ScrollTrigger.refresh()
+
     if(
       catalogContext.models.length > 0 && 
       mainContent.current && mainContent.current.getBoundingClientRect().bottom - window.innerHeight < 100 && 
@@ -51,48 +51,19 @@ export const Catalog = ({searchChoices, models, isQuote}: CatalogProps) => {
     }
   }, [catalogContext.models, catalogContext.isFetching])
 
-  const [isTweening, setIsTweening] = useState(false)
-  const ctx = useRef<gsap.Context>()
-  const ww = typeof window !== 'undefined' ? useWindowSize() : undefined
-  
-  // Animation when more panel is oppen
-  useEffect(() => {
-    ctx.current = gsap.context((self) => {
-      const tl = gsap.timeline({
-        onStart: () => { setIsTweening(true) },
-        onComplete: () => { setIsTweening(false) },
-      })
-
-      self.add('showPanel', () => {
-        if (isTweening) return
-        tl.clear();
-        tl.to(wrapper.current, {
-          x: -window.innerWidth,
-          ease: 'expo',
-          duration: 1.5,
-        })
-      })
-
-      self.add('hidePanel', () => {
-        if (isTweening) return
-        tl.clear()
-        tl.to(wrapper.current, {
-          x: 0,
-          ease: 'expo',
-          duration: 1.5,
-        })
-      })
+  //Blur the background of sideBar on scroll
+  const [backdropBlur, setBackdropBlur] = useState(0)
+  useGSAP(() => {
+    ScrollTrigger.create({
+      trigger: mainContent.current,
+      start: `top top`,
+      end: `top+=${100} top`,
+      id: 'blur',
+      onUpdate: (self) => {
+        setBackdropBlur(self.progress)
+      }
     })
-
-    return () => {
-      ctx.current && ctx.current.revert()
-    }
-  }, [lenis, ww])
-
-  useEffect(() => {    
-    if(catalogContext.showMorePanel) ctx.current && ctx.current.showPanel()
-    else ctx.current && ctx.current.hidePanel()
-  }, [catalogContext.showMorePanel])
+  })
 
   return (
     <div 
@@ -102,28 +73,11 @@ export const Catalog = ({searchChoices, models, isQuote}: CatalogProps) => {
         ref={mainContent}
         className="catalog__main-content relative flex flex-col gap-dashboard-spacing-element-medium w-full"
       >
-        <div className="catalog-searchbar sticky top-0 flex flex-col md:flex-row justify-center w-full gap-4 py-2 z-50 bg-blackBerry-500 backdrop-blur-sm">
-          <SearchBar
-            placeholder="Rechercher"
-            onSearch={() => {}}
-            onChange={() => {}}
-            onFilterClick={() => { setFilterOpen(!filterOpen) }}
-            className='md:w-[500px]'
-            choices={searchChoices}
+        <div className="catalog-searchbar sticky top-0 flex flex-col md:flex-row justify-center w-full gap-4 py-3 z-50">
+          <SearchBarCatalog 
+            searchChoices={searchChoices}
+            blurVal={backdropBlur}
           />
-          <div className="relative">
-            <DropBox
-              title="Filtrer"
-              Icon={FiltersIcon}
-              type="multiple"
-              choices={['Disponible dès demain', 'Carré', '16/9', 'Horizontal']}
-              currentValue={filters}
-              onChange={(val) => { setFilters(val) }}
-              toggle={filterOpen}
-              className="z-40"
-              bgSolid
-            />
-          </div>
         </div>
 
         <div className="catalog-models w-full flex flex-col items-center justify-center gap-dashboard-spacing-element-medium">
@@ -156,12 +110,61 @@ export const Catalog = ({searchChoices, models, isQuote}: CatalogProps) => {
   )
 }
 
+type SearchBarCatalogProps = {
+  searchChoices: CatalogContextType['searchValues'],
+  blurVal: number
+}
+
+const SearchBarCatalog = ({searchChoices, blurVal}:SearchBarCatalogProps) => {
+  const blurryElement = useRef<HTMLDivElement>(null)
+
+  const [filterOpen, setFilterOpen] = useState(false)
+  const [filters, setFilters] = useState<string[]>([])
+
+  useGSAP(() => {
+    gsap.set(blurryElement.current, {
+      backdropFilter: `blur(${blurVal * 4}px)`,
+      opacity: blurVal
+    })
+  }, {dependencies: [blurVal]})  
+
+  return (
+    <>
+      <div 
+        ref={blurryElement}
+        className="absolute w-full h-full top-0 left-0 backdrop-blur bg-blackBerry-500"></div>
+      <SearchBar
+        placeholder="Rechercher"
+        onSearch={() => {}}
+        onChange={() => {}}
+        onFilterClick={() => { setFilterOpen(!filterOpen) }}
+        className='md:w-[500px]'
+        choices={searchChoices}
+      />
+      <div className="relative">
+        <DropBox
+          title="Filtrer"
+          Icon={FiltersIcon}
+          type="multiple"
+          choices={['Disponible dès demain', 'Carré', '16/9', 'Horizontal']}
+          currentValue={filters}
+          onChange={(val) => { setFilters(val) }}
+          toggle={filterOpen}
+          className="absolute z-40"
+          // bgSolid
+        />
+      </div>
+    </>
+  )
+}
+
 type MorePanelProps = {
   isQuote?: boolean
 }
 
 export const MorePanel = forwardRef<HTMLDivElement, MorePanelProps>(function MorePanel({isQuote}, ref) {
   const catalogContext = useContext(CatalogContext)
+  const searchChoices = catalogContext.searchValues
 
   const wrapperRef = ref ? (ref as RefObject<HTMLDivElement>) : useRef<HTMLDivElement>(null)
   const tempScroll = useRef(0)
@@ -199,7 +202,7 @@ export const MorePanel = forwardRef<HTMLDivElement, MorePanelProps>(function Mor
       <ReactLenis
         className="w-full h-screen overflow-scroll no-scroll-bar "
       >
-        <Content isQuote={isQuote}/>
+        <Content isQuote={isQuote} searchChoices={searchChoices}/>
       </ReactLenis>
     </div>
   )
@@ -207,11 +210,12 @@ export const MorePanel = forwardRef<HTMLDivElement, MorePanelProps>(function Mor
 
 type ContentProps = {
   isQuote?: boolean
+  searchChoices: CatalogContextType['searchValues']
 }
 
-const Content = ({isQuote}:ContentProps) => {
+const Content = ({isQuote, searchChoices}:ContentProps) => {
+  const mainContent = useRef<HTMLDivElement>(null)
   const catalogContext = useContext(CatalogContext)
-  const quoteContext = useContext(QuoteContext)
 
   const contentRef = useRef<HTMLDivElement>(null)
 
@@ -226,7 +230,9 @@ const Content = ({isQuote}:ContentProps) => {
   }]
   
   // Trigger the fetch more when we approch the bottom
-  const lenis = useLenis(() => {
+  useLenis(() => {
+    ScrollTrigger.refresh()
+
     if(
       catalogContext.showMorePanel &&
       catalogContext.moreModels.length > 0 && 
@@ -239,31 +245,51 @@ const Content = ({isQuote}:ContentProps) => {
     }
   }, [catalogContext.showMorePanel, catalogContext.moreModels, catalogContext.isFetching])
 
+  //Blur the background of sideBar on scroll
+  const [backdropBlur, setBackdropBlur] = useState(0)
+  useGSAP(() => {
+    const trigger = ScrollTrigger.create({
+      trigger: mainContent.current,
+      start: `top top`,
+      end: `top+=${100} top`,
+      id: 'blur',
+      onUpdate: (self) => {
+        setBackdropBlur(self.progress)
+      }
+    })
+  })
+
   return (
-    <>
-      <Container
-        className="sticky max-w-[1048px] top-[80px] z-20"
-      >
-        <IslandButton
-          type="tertiary"
-          label="Retour"
-          Icon={ArrowLeft} 
-          onClick={() => { catalogContext.setShowMorePanel(false)}}
-          className="w-max align-self-start z-20"
+    <div className="px-[80px]">
+      <div className="catalog-searchbar sticky top-[57px] mt-[50px] flex flex-col md:flex-row justify-center w-full gap-4 py-3 z-20">
+        <SearchBarCatalog 
+          searchChoices={searchChoices}
+          blurVal={backdropBlur}
         />
-      </Container>
+      </div>
 
-
-      <Container>
+      <div
+        ref={mainContent}
+      >
         <div className="bg-blackBerry bg-opacity-80 absolute top-0 left-0 w-full h-full z-0"></div>
         <div 
           ref={contentRef}
-          className="relative z-10 mx-auto py-[80px] ">
+          className="relative z-10 mx-auto ">
 
           <SimpleCard
             className="justify-self-center flex flex-col gap-dashboard-spacing-element-medium mt-dashboard-spacing-element-medium mx-auto"
           >
-            <div className="text-title-small text-dashboard-text-title-white-high w-full text-center">Youtube</div>
+            <div
+              className="flex flex-row justify-between"
+            >
+              <div className="text-title-small text-dashboard-text-title-white-high w-full text-center">Youtube</div>
+              <IslandButton
+                type="tertiary"
+                label="Fermer"
+                onClick={() => { catalogContext.setShowMorePanel(false)}}
+                className="relative w-max align-self-start z-30"
+              />
+            </div>
             <div className="flex flex-col gap-dashboard-spacing-element-medium">
               { linesMore && linesMore.length > 1 && linesMore.map((modelsFormat, i) => {
                 return (
@@ -315,9 +341,8 @@ const Content = ({isQuote}:ContentProps) => {
             </>
             }
           </SimpleCard>
-
         </div>
-      </Container>
-    </>
+      </div>
+    </div>
   )
 }
