7185293f4611a3490a4fcc41d030effd6295cc1c
robinNavas <robinnavas@gmail.com>
2024-01-31 19:02:59 +0100

    correct colors on simple link

--- src/components/_shared/SimpleLink.tsx
+++ src/components/_shared/SimpleLink.tsx
@@ -8,6 +8,6 @@ type SimpleLinkProps = {
 }
 export const SimpleLink = ({href, children, className}:SimpleLinkProps) => {
   return (
-    <Link className={`underline text-opacity-40 hover:text-opacity-60 transition-colors ${className} `} href={href}>{children}</Link>
+    <Link className={`underline text-dashboard-text-button-white-contrast-low text-opacity-40 hover:text-opacity-60 transition-colors ${className ?? ''} `} href={href}>{children}</Link>
   )
 }
