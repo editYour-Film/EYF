94f50c6b13015c4a8a0336310f9d06f5fee0c5fd
robinNavas <robinnavas@gmail.com>
2024-03-04 10:54:41 +0100

    feat: improve seo component

--- src/components/_shared/Seo.tsx
+++ src/components/_shared/Seo.tsx
@@ -5,30 +5,47 @@ type SeoProps = {
 export const Seo = ({ data }: SeoProps) => {
   return (
     <>
-      <title>{data.metaTitle}</title>
-      <meta
-        name="description"
-        content={data.metaDescription}
-        key="description"
-      />
-      <meta name="keywords" content={data.keywords} />
+      {data.metaDescription && (
+        <meta
+          name="description"
+          content={data.metaDescription}
+          key="description"
+        />
+      )}
+      {data.keywords && <meta name="keywords" content={data.keywords} />}
 
       {data.metaSocial &&
         data.metaSocial.map((el: any) => {
           if (el.socialNetwork === "Facebook") {
             return (
               <>
-                <meta
-                  property="og:url"
-                  content={data.canonicalURL}
-                  key="og:url"
-                />
-                <meta property="og:title" content={el.title} key="og:title" />
-                <meta
-                  property="og:description"
-                  content={el.description}
-                  key="og:description"
-                />
+                {data.canonicalURL && (
+                  <meta
+                    property="og:url"
+                    content={data.canonicalURL}
+                    key="og:url"
+                  />
+                )}
+
+                {el.title && (
+                  <meta property="og:title" content={el.title} key="og:title" />
+                )}
+
+                {el.description && (
+                  <meta
+                    property="og:description"
+                    content={el.description}
+                    key="og:description"
+                  />
+                )}
+
+                {el.image && (
+                  <meta
+                    property="og:image"
+                    content={el.image.data.attributes.formats.medium.url}
+                    key="og:image"
+                  />
+                )}
               </>
             );
           } else if (el.socialNetwork === "Twitter") {
@@ -39,19 +56,27 @@ export const Seo = ({ data }: SeoProps) => {
                   content="summary_large_image"
                   key="twitter:card"
                 />
-                <meta name="twitter:site" content={data.canonicalURL} />
+                {data.canonicalURL && (
+                  <meta name="twitter:site" content={data.canonicalURL} />
+                )}
+
                 <meta name="twitter:creator" content="@editYourFilm" />
-                <meta name="twitter:title" content={el.title} />
-                <meta name="twitter:description" content={el.description} />
-                {/* <meta name="twitter:image" content={data.metaImage.data.attributes.formats.medium} /> */}
+                {el.title && <meta name="twitter:title" content={el.title} />}
+                {el.description && (
+                  <meta name="twitter:description" content={el.description} />
+                )}
+                {el.image && (
+                  <meta
+                    name="twitter:image"
+                    content={el.image.data.attributes.formats.medium.url}
+                  />
+                )}
               </>
             );
           }
         })}
 
-      {/* <meta property="og:image" content={data.metaImage.data.attributes.formats.medium} key="og:image" /> */}
-
-      <link rel="canonical" href={data.canonicalURL} />
+      {data.canonicalURL && <link rel="canonical" href={data.canonicalURL} />}
 
       {data.metaRobots && (
         <>
