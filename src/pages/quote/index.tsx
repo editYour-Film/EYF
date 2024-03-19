7946dceaee1be07fbfabf1de9bb767a98038c9c0
robinNavas <robinnavas@gmail.com>
2024-03-19 10:34:53 +0100

    feat: adds typeform to the quote page

--- src/pages/quote/index.tsx
+++ src/pages/quote/index.tsx
@@ -1,8 +1,20 @@
+import LayoutMain from "@/components/layouts/LayoutMain";
 import LayoutQuote from "@/components/layouts/LayoutQuote";
 import { QuoteContent } from "@/components/quote/QuoteContent";
+import { disableCustomCursor } from "@/store/slices/cursorSlice";
 import Head from "next/head";
+import Script from "next/script";
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { Widget } from "@typeform/embed-react";
 
 export default function Duration() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(disableCustomCursor());
+  }, []);
+
   return (
     <>
       <Head>
@@ -10,9 +22,22 @@ export default function Duration() {
         <meta name="description" content="" />
       </Head>
 
-      <LayoutQuote>
+      {/* <LayoutQuote>
         <QuoteContent />
-      </LayoutQuote>
+      </LayoutQuote> */}
+
+      <LayoutMain>
+        <div className="w-full h-[100vh]">
+          <Widget
+            width="100%"
+            height="100%"
+            fullScreen={true}
+            id="WVu9Lcrc"
+            style={{ width: "50%" }}
+            className="my-form"
+          />
+        </div>
+      </LayoutMain>
     </>
   );
 }
